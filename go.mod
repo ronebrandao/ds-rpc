4aa262d52328b991b2328bebf8983efafb3d10e5
Rone Filho <rone@approx.local>
2020-05-13 23:35:48 -0300

    Simple add and multiply routes using rpc

---
+++ go.mod
@@ -0,0 +1,15 @@
+module ds-rpc
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/golang/protobuf v1.4.1
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
+	golang.org/x/sys v0.0.0-20200511232937-7e40ca221e25 // indirect
+	google.golang.org/genproto v0.0.0-20200511104702-f5ebc3bea380 // indirect
+	google.golang.org/grpc v1.29.1
+	google.golang.org/protobuf v1.22.0
+)
---
+++ server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"ds-rpc/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+type server struct {
+
+}
+
+func main() {
+ 	listener, err := net.Listen("tcp", ":4040")
+ 	if err != nil {
+ 		panic(err)
+	}
+
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &server{})
+	reflection.Register(srv)
+
+	if err = srv.Serve(listener); err != nil {
+		panic(err)
+	}
+}
+
+func (s* server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a + b
+
+	return &proto.Response{Result: result}, nil
+}
+
+func (s* server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a * b
+
+	return &proto.Response{Result: result}, nil
+}
+
+
